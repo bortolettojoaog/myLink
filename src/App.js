290d3df1121167709a7984a95cdec55f21eb921c
João G. Bortoletto <jgb3532@hotmail.com>
2024-08-22 19:09:38 -0300

    feat: start using routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import RoutesApp from "./routes";
+
 export default function App() {
-    return <div className="app"></div>;
+    return (
+        <div className="app">
+            <RoutesApp />
+        </div>
+    );
 }
