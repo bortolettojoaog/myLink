e762453ec8f1b8f48215d9b5d04101f8ed75582b
João G. Bortoletto <jgb3532@hotmail.com>
2024-08-22 19:05:56 -0300

    feat: create home interface

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import { FiLink } from "react-icons/fi";
 import "./home.css";
 
+import Menu from "../../components/Menu";
+
 export default function Home() {
-    return <div>Home</div>;
+    const [link, setLink] = useState("");
+
+    return (
+        <div className="container-home">
+            <div className="logo">
+                <img src="/logo.png" alt="Meu Link Logo" />
+                <h1>Meu Link</h1>
+                <span>Cole seu link para encurtar! 👇</span>
+            </div>
+
+            <div className="area-input">
+                <div>
+                    <FiLink size={24} color="#FFF" />
+                    <input
+                        placeholder="Cole seu link aqui..."
+                        type="text"
+                        value={link}
+                        onChange={(e) => setLink(e.target.value)}
+                    />
+                </div>
+
+                <button>Gerar Link</button>
+            </div>
+
+            <Menu />
+        </div>
+    );
 }
